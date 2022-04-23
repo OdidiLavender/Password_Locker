94eb4ca3c3f48cbc50d0bc595b2a454965b7fdbb
OdidiLavender <odidilavender@gmail.com>
2022-04-23 16:33:17 +0300

    created a save function and the delete function of the user

--- user.py
+++ user.py
@@ -1,10 +1,21 @@
-class user
-'''
-user class generates new instance of the user
-'''
-userlist = [] #Empty userlist
+class User:
+    '''
+    user class generates new instance of the user
+    '''
+    userlist = [] #Empty userlist
 def __init__ (self,Username,Password):
     self.Username = Username
     self.Password = Password
 
+def save_user(self):
+    '''
+    save user saves the new user to the userlist
+    '''
+    User.userlist.append(self)
+
+def delete_user(self):
+    '''
+    deletes user from the userlist
+    '''
+    User.userlist.remove(self)
 
