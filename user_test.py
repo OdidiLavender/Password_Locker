5367cbf5de9f3fa704a5cc57c182c7f3ed5c1f39
OdidiLavender <odidilavender@gmail.com>
2022-04-23 17:10:51 +0300

    added the test for save user that will generate new object

--- user_test.py
+++ user_test.py
@@ -15,9 +15,15 @@ def test_init(self):
     self.assertEqual(self.new_user.Username,"Lavender")
     self.assertEqual(self.new_user.Password,"1234")
 
+def test_save_user(self):
+    '''
+    test_save_user test case to test if the user object is saved into
+    the userlist
+    '''
+    self.new_user.save_user() # saving the new user
+    self.assertEqual(len(User.userlist),1)
 
 
-    
 if __name__ == '__main__':
     unittest.main()    
     
