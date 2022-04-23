40f036ec1d662c07631574a07064de8a88472dde
OdidiLavender <odidilavender@gmail.com>
2022-04-23 16:23:47 +0300

    created a user class

---
+++ user.py
@@ -0,0 +1,10 @@
+class user
+'''
+user class generates new instance of the user
+'''
+userlist = [] #Empty userlist
+def __init__ (self,Username,Password):
+    self.Username = Username
+    self.Password = Password
+
+
