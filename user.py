164cb4dde68b6d37c0e67f9568a9b47cf8ee18ec
OdidiLavender <odidilavender@gmail.com>
2022-04-24 18:38:14 +0300

    defined functions for every step of the application

--- user.py
+++ user.py
@@ -18,4 +18,72 @@ def delete_user(self):
     deletes user method from the userlist
     '''
     User.userlist.remove(self)
+@classmethod
+def find_by_number(cls,number):
+    '''
+    Method that takes in username and returns a user that matches that number.
+        
+    '''
+    for user in cls.userlist:
+        if user.password == number:
+            return user
+@classmethod
+def user_exists(cls,number):
+    '''
+    method checks if the username exists
+    '''
+    for user in cls.userlist:
+        if user.username == number:
+            return True #return true if there is same username
+
+            return False #return false if no username has the same number
+
+
+
+@classmethod
+def display_user(cls):
+    '''
+    method returns the userlist
+    '''
+    return cls.userlist
+
+
+class Credentials:
+    '''
+    class generates new instance of credentials
+    '''
+    account = []
+    def __init__(self,accountname,accountusername,accountpassword):
+        '''
+        init method defines properties for our object
+        '''
+        self.accountname = accountname
+        self.accountusername = accountusername
+        self.accountpassword = accountpassword
+    
+    def save_account(self):
+        '''
+        this method saves the user credentials in account
+        '''
+        Credentials.account.append(self)
+
+    def delete_account(self):
+        '''
+        this method delete user credentials from account 
+        '''
+        Credentials.account.remove(self)
+
+@classmethod
+def display_account(cls):
+    '''
+    method returns a list of the accounts
+    '''
+    for account in cls.accounts:
+        return cls.accounts
+
+@classmethod
+def find_by_number(cls,number):
+    for account in cls.accounts:
+        if account.accountname == number:
+            return account
 
