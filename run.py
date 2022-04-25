a4874db823f065f0c70b372295b34c0b358ca2db
OdidiLavender <odidilavender@gmail.com>
2022-04-25 20:14:56 +0300

    updated all files

--- run.py
+++ run.py
@@ -1,16 +1,230 @@
+import random
 import string
-from random import *
-from user import User
-from user import Credentials
+import pyperclip
 
-def create_user(firstname,lastname,username,userpassword):
-    new_user= User(firstname,lastname,username,userpassword)
-    return new_user
-def save_user(user):
-    user.save_user()
-def delete_user(user):
-    user.delete_user()
-def find_user(number):
-    return User.find_by_number(number)
-def display_user():
-    return User.display_user()
+class User:
+    user_list= []
+    def __init__(self,user_name, user_password):
+        self.user_name = user_name
+        self.user_password = user_password
+        self.isLogggedin =False
+
+    def createUser(user_name, user_password):
+        '''
+        function to create new user
+        '''
+        new_user = User(user_name, user_password)
+        return new_user
+    def login(self):
+        '''
+        method that allows user to login credentials
+        '''
+        print("Hello welcome to PASSWORD-LOCKER...")
+    def saveUser(self):
+        """
+        method that adds user to the list
+        """
+        User.user_list.append(self)
+    @classmethod
+    def displayUser(cls):
+        """
+        method that displays saved users
+        """
+        return cls.user_list
+    def deleteUser(self):
+        """
+        method that delete a selected user
+        """
+        User.user_list.remove(self)
+if __name__ == "__main__":
+    pass
+class Credentials:
+    credentials = []
+    def __init__(self, accountName, accountUsername, accountPassword):
+        """
+        crede
+        """
+        self.accountName = accountName
+        self.accountUsername = accountUsername
+        self.accountPassword = accountPassword
+    def saveCredential(self):
+        """
+        method that adds credentials to the list
+        """
+        Credentials.credentials.append(self)
+    @classmethod
+    def createCredential(self, accountName, accountUsername, accountPassword):
+        """
+        method that creates a new credential
+        """
+        newCredential = Credentials(
+            accountName, accountUsername, accountPassword)
+        return newCredential
+    def searchCredential(accountName):
+        """
+        method that searches a new credential and returns the credential
+        """
+        if Credentials.credentials:
+            for credential in Credentials.credentials:
+                if credential.accountName == accountName:
+                    return credential
+            print("No such an account")
+        else:
+            print("No credentials saved")
+    def displayCredential():
+        """
+        methods to display all saved credentials
+        """
+        if (len(Credentials.credentials) > 0):
+            return Credentials.credentials
+    @classmethod
+    def credentialExist(self, accountName):
+        """
+        method that checks if a credentials exists
+        """
+        if Credentials.credentials:
+            for credential in Credentials.credentials:
+                if credential.accountName == accountName:
+                    return True
+            print("No such an account")
+        else:
+            print("No credentials saved")
+    def deleteCredential(accountName):
+        """
+        method that deletes a credential
+        """
+        for credential in Credentials.credentials:
+            if credential.accountUsername == accountName:
+                Credentials.credentials.remove(credential)
+    def passwordGenerate(stringLength=8):
+        """
+        method that generate a random password
+        """
+        password = string.ascii_lowercase + string.ascii_uppercase + "abcd*1245%"
+        return ''.join(random.choice(password) for i in range(int(stringLength)))
+    def copypassword(parameter_list):
+        """
+        method that allows copying of password to keyboard
+        """
+        pass
+if __name__ == "__main__":
+
+    isTrue = True
+    print("welcome .This is a python application that manages our passwords and even create new passwords for us.")
+    while isTrue == True:
+        print(
+            "Use these short code to proceed:\n\n new-Create new Account\n lgn-Login\n ext-Exit")
+        shortCode = input().lower()
+        if shortCode == 'new':
+            print("New Account")
+          
+            print("Username")
+            user_name = input()
+            while True:
+                print(
+                    "own - own password?\n ran - generate random password")
+                passwordOption = input()
+                if passwordOption == 'own':
+                    print("Your Password")
+                    password = input()
+                    break
+                elif passwordOption == 'ran':
+                    password = Credentials.passwordGenerate()
+                    break
+                else:
+                    print("PasswordOption Invalid")
+            createUser = User.createUser(user_name, password)
+            User.saveUser(createUser)
+            print("\n")
+            print(
+                f"Hello {user_name} Your Account has been created sucessfully\n")
+            print(f"Your username is {user_name} and password is {password}\n")
+        elif shortCode == 'lgn':
+            print("*"*3)
+            print("Enter your username and password")
+            print("*"*10)
+            print("Username")
+            user_name = input()
+            print("Password")
+            user_password = input()
+            for user in User.user_list:
+                if user_name == user.user_name:
+                    if user.password == user_password:
+                        print(user.login())
+                    else:
+                        print("password invalid")
+                        break
+                else:
+                    print("username Invalid")
+                    break
+            break
+        elif shortCode == 'ext':
+            print("Goodbye. See you again!")
+            break
+        else:
+            print("Invalid shortcode\n")
+    while True:
+        print("Would you like to procced?\n\n Use these shortcodes to proceed\n sav - Save new Credential \n shw - Display existing Credential\n see.acc - Find a credential \n del.acc -  Delete an existing Credential \n ext - Exit Application")
+        shortCode = input().lower()
+        if shortCode == 'sav':
+            print("New Credential Account")
+            print("*"*50)
+            print("\n")
+            print("Account Name e.g Facebook")
+            AccountName = input()
+            print("Account's UserName")
+            accountUserName = input()
+            while True:
+                print(
+                    "my - You want to type your own password?\n ran -  generate random password")
+                PasswordOption = input().lower()
+                if PasswordOption == 'own':
+                    print("Account's Password :")
+                    accountPassword = input()
+                    break
+                elif PasswordOption == 'ran':
+                    accountPassword = Credentials.passwordGenerate()
+                    break
+                else:
+                    print("Password Invalid")
+            newC = Credentials.createCredential(
+                AccountName, accountUserName, accountPassword)
+            Credentials.saveCredential(newC)
+            print("\n")
+            print("Your Account's Credentials has been saved!")
+            print("\n")
+
+
+        elif shortCode == 'shw':
+            if Credentials.displayCredential():
+                print("List of your credentials include:\n")
+                for credential in Credentials.credentials:
+                    accountName = credential.accountName
+                    accountuser = credential.accountUsername
+                    accountpass = credential.accountPassword
+                    print(
+                        f"Account Name : {accountName}\n Account Username : {accountuser}\n Account Password: {accountpass}\n")
+            else:
+                print("You do not have saved credentials at the moment\n")
+        elif shortCode == 'see.acc':
+            print("Account name: ")
+            AccountName = input()
+            if Credentials.credentialExist(AccountName):
+                searchAccount = Credentials.searchCredential(AccountName)
+                print(
+                    f"Account name: {searchAccount.accountName}\n Account's Username: {searchAccount.accountUsername}\n Account's Password : {searchAccount.accountPassword}")
+            else:
+                print("No such an account name!\n")
+        elif shortCode == 'del.acc':
+            print("Account name you would like to delete?")
+            AccountName = input()
+            if Credentials.credentialExist(AccountName):
+                Credentials.deleteCredential(AccountName)
+                print("Account was deleted Successfully")
+            else:
+                print("No such an account name")
+        elif shortCode == 'ext':
+            print("bye")
+            isTrue = False
+        else:
+            print("invalid shortcode")     
--- user.py
+++ user.py
@@ -1,89 +1,24 @@
 class User:
-    '''
-    user class generates new instance of the user
-    '''
-    userlist = [] #Empty userlist
-def __init__ (self,Username,Password):
-    self.Username = Username
-    self.Password = Password
+    """
+    class that generates new instance of user
+    """
 
-def save_user(self):
-    '''
-    save user method saves the new user to the userlist
-    '''
-    User.userlist.append(self)
-
-def delete_user(self):
-    '''
-    deletes user method from the userlist
-    '''
-    User.userlist.remove(self)
-@classmethod
-def find_by_number(cls,number):
-    '''
-    Method that takes in username and returns a user that matches that number.
-        
-    '''
-    for user in cls.userlist:
-        if user.password == number:
-            return user
-@classmethod
-def user_exists(cls,number):
-    '''
-    method checks if the username exists
-    '''
-    for user in cls.userlist:
-        if user.username == number:
-            return True #return true if there is same username
-
-            return False #return false if no username has the same number
-
-
-
-@classmethod
-def display_user(cls):
-    '''
-    method returns the userlist
-    '''
-    return cls.userlist
-
-
-class Credentials:
-    '''
-    class generates new instance of credentials
-    '''
-    account = []
-    def __init__(self,accountname,accountusername,accountpassword):
+    user_list =[] #Empty user list
+     #Init method up here
+    def save_user(self,user_name,user_password):
         '''
-        init method defines properties for our object
+        save user method saves user object into user_list
         '''
-        self.accountname = accountname
-        self.accountusername = accountusername
-        self.accountpassword = accountpassword
-    
-    def save_account(self):
+        User.user_list.append(self)
+    def delete_user(self,user_name,user_password):
         '''
-        this method saves the user credentials in account
+        delete_user method deletes a saved user from the user_list
         '''
-        Credentials.account.append(self)
+        User.user_list.remove(self)
 
-    def delete_account(self):
-        '''
-        this method delete user credentials from account 
-        '''
-        Credentials.account.remove(self)
+    def __init__(self,user_name,user_password):
 
-@classmethod
-def display_account(cls):
-    '''
-    method returns a list of the accounts
-    '''
-    for account in cls.accounts:
-        return cls.accounts
+      # docstring removed for simplicity
 
-@classmethod
-def find_by_number(cls,number):
-    for account in cls.accounts:
-        if account.accountname == number:
-            return account
-
+        self.user_name=user_name
+        self.user_password=user_password
